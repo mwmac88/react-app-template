df2e9280a7c1c9412d34f6acfac0cbdfb0a04b7d
Matt Maclean <mwmaclean88@gmail.com>
2019-08-27 20:25:28 +0100

    feat: add testing facilities & remove css imports

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import './app.css';
-import Header from '../header';
 
 function App() {
   return (
-    <div className="app">
-      <Header />
+    <div class="app">
+      { 'Hello World!' }
     </div>
   );
 }
